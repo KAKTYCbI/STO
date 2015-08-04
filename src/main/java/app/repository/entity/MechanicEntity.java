0c766ef908913d7266154809d63e1678ada291c1
Oleg <shmyger_oleg@mail.ru>
2015-08-04 13:23:03 +0300

    add entity mechanic

---
+++ src/main/java/app/repository/entity/MechanicEntity.java
@@ -0,0 +1,83 @@
+package app.repository.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class MechanicEntity {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="mechanic_id")
+	private Long mechanicId;
+	
+	@Column(name="name")
+	private String name;
+	
+	@Column(name="password")
+	private String password;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="contact")
+	private String contact;
+	
+	@Column(name="rating")
+	private Double rating;
+
+	public Long getMechanicId() {
+		return mechanicId;
+	}
+
+	public void setMechanicId(Long mechanicId) {
+		this.mechanicId = mechanicId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getContact() {
+		return contact;
+	}
+
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
+
+	public Double getRating() {
+		return rating;
+	}
+
+	public void setRating(Double rating) {
+		this.rating = rating;
+	}
+
+	
+}
