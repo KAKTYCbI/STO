ad04533f3eb4b9af3fdb3e1db76b6b4e7402cc8b
akhramenkosergey <qwerty_1111@tut.by>
2015-08-12 00:09:26 +0300

    add repository layer

--- src/main/java/app/web/LoginController.java
+++ src/main/java/app/web/LoginController.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,18 +13,29 @@ import org.springframework.web.servlet.ModelAndView;
 
 import domain.service.UserService;
 
-
-
 @Controller
 @RequestMapping("")
 public class LoginController {
 
 	@Autowired
 	private UserService userService;
-	
+
 	@RequestMapping(value = { "", "/", "/login" }, method = { RequestMethod.GET })
 	public String login() {
 		return "login";
 	}
-	
+
+	@RequestMapping(value = { "/access-denied" }, method = { RequestMethod.GET })
+	public String denied() {
+		return "denied"; // надо сделать!!
+	}
+
+	@RequestMapping(value = "/login/failure", method = RequestMethod.GET)
+	public String loginerror(ModelMap model) {
+
+		model.addAttribute("error");
+		return "login";
+
+	}
+
 }
--- src/main/java/domain/service/UserService.java
+++ src/main/java/domain/service/UserService.java
@@ -1,12 +1,27 @@
 package domain.service;
 
+import java.util.List;
+
 import domain.model.UserPrincipal;
 
 public interface UserService {
 	
-	UserPrincipal loadUserByCredentials(String login, String password);
+	 // Авторизация пользователя на сайте
+	 UserPrincipal getUser(String login, String password);
+	 
+	 //возвращает пользователя по ИД
+	 UserPrincipal getUserByID(Long userId);
+	 
+	 //возвращает пользователя по имени
+	 UserPrincipal getUserByName(String name);
+	 
+	 // метод возвращает список всех клиентов
+	 List<UserPrincipal> getUsers();
+	 
+	 // сохранение введенной информации об аккаунте от клиента
+	 void saveUser(UserPrincipal user);
 
-	UserPrincipal getUserByID(Long userId);
+	 // удаление выбранного клиента
+	 void deleteUser(UserPrincipal user);
 
-	UserPrincipal getUserByName(String username);
 }
