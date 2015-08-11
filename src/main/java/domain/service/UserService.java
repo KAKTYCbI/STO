50fc6a20a4d1559e53e188d6d16578a9d7bdb8db
Oleg <shmyger_oleg@mail.ru>
2015-08-12 00:21:31 +0300

    first commit

--- src/main/java/domain/service/UserService.java
+++ src/main/java/domain/service/UserService.java
@@ -4,9 +4,15 @@ import domain.model.UserPrincipal;
 
 public interface UserService {
 	
-	UserPrincipal loadUserByCredentials(String login, String password);
+	UserPrincipal getUser(String login, String password);
 
 	UserPrincipal getUserByID(Long userId);
 
 	UserPrincipal getUserByName(String username);
+	
+	// сохранение введенной информации об аккаунте от клиента
+    void saveUser(UserPrincipal user);
+
+	// удаление выбранного клиента
+	void deleteUser(UserPrincipal user);
 }
